90ed18a76ec3e588686d675d9f3f058814ab4dc2
Tim <tim.fischer@artorg.unibe.ch>
2019-04-10 15:58:01 +0200

    added exceptions (error handling) renamed networks to FC (Fully Connected)

--- ProjectCode/nn_models.py
+++ ProjectCode/nn_models.py
@@ -1,26 +1,23 @@
 # Here we define our network classes
-
 import torch
 import torch.nn as nn
 
-dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-
-# Simple Linear NN
-class simpleANN(nn.Module):
+# Fully Connected (FC) network
+class SimpleFC(nn.Module):
     def __init__(self):
         super().__init__()
         self.layers = nn.Sequential(
             nn.Linear(36 * 36, 2, bias=True),
         )
 
-    def forward(self, input):
-        input = input.view(input.size(0), -1)
-        return self.layers(input)
+    def forward(self, x):
+        output = x.view(x.size(0), -1)
+        return self.layers(output)
 
 
-# Deep NN. Not so deep though.
-class deepANN(nn.Module):
+# Fully Connected (FC) little deep network
+class DeepFC(nn.Module):
     def __init__(self):
         super().__init__()
         self.layers = nn.Sequential(
@@ -31,24 +28,26 @@ class deepANN(nn.Module):
             nn.Linear(128, 2, bias=True)
         )
 
-    def forward(self, input):
-        input = input.view(input.size(0), -1)
-        return self.layers(input)
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        return self.layers(x)
 
 
-# Model selecter, SGD parameter set.
+# Model selection
 def get_nn_model(model_name):
-    if model_name == 'simple':
-        model = simpleANN()
-    elif model_name == 'deep':
-        model = deepANN()
+    if model_name == 'SimpleFC':
+        model = SimpleFC()
+    elif model_name == 'DeepFC':
+        model = DeepFC()
     else:
-        print('Error at slecting model, choosing simple model instead.')
-        model = simpleANN()
-    print(dev, "will be used for training of this model.")
-    if (dev == torch.device("cuda")) and (torch.cuda.device_count() > 1):
+        raise Exception('Mismatch between nn_list in main.py and available names in get_nn_model')
+
+    # Transfer model to gpu if possible
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    print(device, "Will be used for training of this model.")
+    if (device == torch.device("cuda")) and (torch.cuda.device_count() > 1):
         print("You have", torch.cuda.device_count(), "GPUs for training!")
         model = nn.DataParallel(model)
+    model = model.to(device)
 
-    model = model.to(dev)
     return model
