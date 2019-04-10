0229c95591e9c5f42b8439f628f5a4cc3c6ca0cb
Tim <tim.fischer@artorg.unibe.ch>
2019-04-10 15:13:44 +0200

    added early stopping criteria to fit function

--- ProjectCode/utils_train.py
+++ ProjectCode/utils_train.py
@@ -50,25 +50,43 @@ def test(model, test_loader, loss_fn):
     return average_loss, accuracy
 
 
-def fit(train_dataloader, val_dataloader, model, optimizer, loss_fn, n_epochs, scheduler=None):
-    tStart = time.time()
-    
+def fit(train_dataloader, val_dataloader, model, optimizer, loss_fn, n_epochs, scheduler=None, apply_early_stopping=False):
     train_losses, train_accuracies = [], []
     val_losses, val_accuracies = [], []
 
+    if apply_early_stopping:
+        best_val_loss = np.inf
+        best_model = None
+        patience = 5  # if no improvement after 5 epochs, stop training
+        counter = 0
+
     for epoch in range(n_epochs):
+        print('learning_rate', optimizer.param_groups[0]['lr'])
         train_loss, train_accuracy = train(model, train_dataloader, optimizer, loss_fn)
         val_loss, val_accuracy = test(model, val_dataloader, loss_fn)
         train_losses.append(train_loss)
         train_accuracies.append(train_accuracy)
         val_losses.append(val_loss)
         val_accuracies.append(val_accuracy)
+        # We'll monitor learning rate -- just to show that it's decreasing
         if scheduler:
-            scheduler.step() # argument only needed for ReduceLROnPlateau
-        print('Epoch {}/{}: train_loss: {:.4f}, train_accuracy: {:.4f}, val_loss: {:.4f}, val_accuracy: {:.4f}'.format(epoch+1, n_epochs,
-                                                                                                          train_losses[-1],
-                                                                                                          train_accuracies[-1],
-                                                                                                          val_losses[-1],
-                                                                                                          val_accuracies[-1]))
-    print("\nTime spent for training: ",time.time()-tStart)
-    return train_losses, train_accuracies, val_losses, val_accuracies
+            scheduler.step()  # argument only needed for ReduceLROnPlateau
+        print('Epoch {}/{}: train_loss: {:.4f}, train_accuracy: {:.4f}, val_loss: {:.4f}, val_accuracy: {:.4f}'.format(
+            epoch + 1, n_epochs,
+            train_losses[-1],
+            train_accuracies[-1],
+            val_losses[-1],
+            val_accuracies[-1]))
+        if apply_early_stopping:
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                best_model = deepcopy(model)
+                counter = 0
+            else:
+                counter += 1
+            if counter == patience:
+                print('No improvement for {} epochs; training stopped.'.format(patience))
+                model = best_model
+                break
+
+    return train_losses, train_accuracies, val_losses, val_accuracies, model
