1ce1876785559acd27c1ad0782caab5e2fd0291b
Özhan Özen <oezhan.oezen@artorg.unibe.ch>
2019-04-26 21:27:24 +0200

    Fix regarding multiple model plots.
    Was deleting previous metrics before.

--- ProjectCode/configs/configs_ozhan.py
+++ ProjectCode/configs/configs_ozhan.py
@@ -32,8 +32,39 @@ class ConfigNo01(DefaultConfig):
     verbose = 'CRITICAL'
     
     batch_size = 128
+    
+class ConfigNo02(DefaultConfig):
+    # Overwriting base class attributes
+    num_of_epochs = 30
+
+    # Give it a unique name and a brief description if you like
+    config_name = 'simpleNN'
+    config_remark = 'This is a simple NN test.. nothing serious'
+    selected_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    
+    
+    show_events_distribution = False
+    show_eeg_sample_plot = False
+    subjectIdx_to_plot = 1
+    seconds_to_plot = 3
+    channels_to_plot = 5
+    
+    nn_list = ['SimpleFC', 'DeepFC']  # Extend if you want more. Add them in the nn_models_getter.py module
+    nn_selection_idx = 1
+
+    # Select optimizer parameters
+    
+    learning_rate = 1e-3
+    weight_decay = 0.005
+    momentum = 0.01  # Relevant only for SGDMomentum, else: ignored
+    optimizer_list = ['Adam', 'SGD', 'SGDMomentum']  # Extend if you want more. Add them in the optimizers.py module
+    optimizer_selection_idx = 0  # Idx corresponds to entry optimizer_list (find below)
+    
+    verbose = 'CRITICAL'
+    
+    batch_size = 128
 
 
 
 # Put them all in a list
-list_of_configs = [ConfigNo01]
+list_of_configs = [ConfigNo01, ConfigNo02]
