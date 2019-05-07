7cc0e353bf615481fbd9622407bdbb61fad9ca2b
Tim <tim.fischer@artorg.unibe.ch>
2019-05-07 13:37:59 +0200

    further braindecode progress

--- ProjectCode/optimizers.py
+++ ProjectCode/optimizers.py
@@ -1,6 +1,6 @@
 # Here we define our optimizers
 from torch import optim
-
+from braindecode.torch_ext.optimizers import AdamW
 
 def get_optimizer(optimizer_name, learning_rate, model_parameters, sgd_momentum=0, weight_decay_factor=0):
     if optimizer_name == 'Adam':
@@ -10,6 +10,8 @@ def get_optimizer(optimizer_name, learning_rate, model_parameters, sgd_momentum=
     elif optimizer_name == 'SGDMomentum':
         optimizer = optim.SGD(model_parameters, lr=learning_rate, momentum=sgd_momentum,
                               weight_decay=weight_decay_factor)
+    elif optimizer_name == 'AdamW':
+        optimizer = AdamW(model_parameters, lr=learning_rate, weight_decay=weight_decay_factor)
     else:
         raise Exception('Mismatch between optimizer_list in main.py and available names in get_optimizer')
     return optimizer
--- ProjectCode/tim_braindecode.py
+++ ProjectCode/tim_braindecode.py
@@ -1,16 +1,24 @@
-from neural_nets.nn_models_getter import get_nn_model
-from optimizers import get_optimizer
-from utils_train import fit, test
+
 from configs import configs_tim
 from data_loader_creation import get_dataloader_objects
-from classification_results import results_storer
-from visualisations import plot_metrics_from_pkl
-
+from optimizers import get_optimizer
+import torch
+import torch.nn.functional as F
 """ USER: SELECT THE CONFIGURATION YOU NEED """
 myList = configs_tim.list_of_configs
 # myList = configs_joaquin.list_of_configs
 # myList = configs_ozhan.list_of_configs
+import logging
+import importlib
+import sys
+import numpy as np
+from braindecode.torch_ext.util import set_random_seeds
 
+
+if torch.cuda.is_available():
+    cuda = True
+else:
+    cuda = False
 for idx, my_cfg in enumerate(myList):
     print('++++ CONFIGURATION %2d, of %2d' % (idx, len(myList)))
 
@@ -20,47 +28,80 @@ for idx, my_cfg in enumerate(myList):
     train_dl, val_dl, test_dl, input_dimension_, output_dimension_ = get_dataloader_objects(my_cfg)
 
     """CLASSIFICATION"""
-    # allow logging
-    import logging
-    import importlib
-
     importlib.reload(logging)  # see https://stackoverflow.com/a/21475297/1469195
     log = logging.getLogger()
     log.setLevel('INFO')
-    import sys
-
     logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s',
                         level=logging.INFO, stream=sys.stdout)
 
 
-    from braindecode.models.shallow_fbcsp import ShallowFBCSPNet
-    from torch import nn
-    from braindecode.torch_ext.util import set_random_seeds
-
-
-
-
-    # Set if you want to use GPU
-    # You can also use torch.cuda.is_available() to determine if cuda is available on your machine.
-    cuda = False
+    # Set if you want to use GPU.
     set_random_seeds(seed=20170629, cuda=cuda)
     n_classes = 10
     in_chans = train_dl.dataset.data.shape[1]
-    # final_conv_length = auto ensures we only get a single output in the time dimension
-    model = ShallowFBCSPNet(in_chans=in_chans, n_classes=n_classes,
-                            input_time_length=train_dl.dataset.data.shape[2],
-                            final_conv_length='auto')
+    # Enable Logging
+    importlib.reload(logging)  # see https://stackoverflow.com/a/21475297/1469195
+    log = logging.getLogger()
+    log.setLevel('INFO')
+    logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s',
+                        level=logging.INFO, stream=sys.stdout)
+    from braindecode.models.shallow_fbcsp import ShallowFBCSPNet
+    from braindecode.models.deep4 import Deep4Net
+    from braindecode.models.eegnet import Conv2dWithConstraint, EEGNet, EEGNetv4
+    from braindecode.models.hybrid import HybridNetModule
+    """ BUILD THE MODEL"""
+    cropped = False
+    n_epochs = 1
+
+    if cropped:
+        """For cropped decoding, we now transform the model into a model that outputs a dense time series of 
+        predictions. For this, we manually set the length of the final convolution layer to some length that makes the 
+        receptive field of the ConvNet smaller than the number of samples in a trial (see final_conv_length=12 in the
+        model definition)."""
+        final_conv_length_param = 12
+        model = ShallowFBCSPNet(in_chans=in_chans, n_classes=n_classes,
+                                input_time_length=None,
+                                final_conv_length=final_conv_length_param)
+    else:
+        model = ShallowFBCSPNet(in_chans=in_chans, n_classes=n_classes,
+                                input_time_length=train_dl.dataset.data.shape[2],
+                                final_conv_length='auto')
+
+        model = Deep4Net(in_chans=in_chans, n_classes=n_classes,
+                         input_time_length=train_dl.dataset.data.shape[2],
+                         final_conv_length='auto')
+        """
+        model = Conv2dWithConstraint(in_channels=in_chans,kernel_size=5, out_channels=output_dimension_)
+        model = EEGNet(in_chans=in_chans, n_classes=n_classes,
+                         input_time_length=train_dl.dataset.data.shape[2],
+                         final_conv_length='auto')
+        model = EEGNetv4(in_chans=in_chans, n_classes=n_classes,
+                         input_time_length=train_dl.dataset.data.shape[2],
+                         final_conv_length='auto')
+        model = HybridNetModule(in_chans=in_chans, n_classes=n_classes,
+                         input_time_length=train_dl.dataset.data.shape[2])
+"""
+
+
+
+
     if cuda:
         model.cuda()
 
-    from braindecode.torch_ext.optimizers import AdamW
-    import torch.nn.functional as F
-    #optimizer = AdamW(model.parameters(), lr=1*0.01, weight_decay=0.5*0.001) # these are good values for the deep model
-    optimizer = AdamW(model.parameters(), lr=0.0625 * 0.01, weight_decay=0)
-    model.compile(loss=F.nll_loss, optimizer=optimizer, iterator_seed=1,)
+    optimizer = get_optimizer('AdamW', learning_rate=0.0625 * 0.01, model_parameters=model.parameters(), sgd_momentum=0,
+                              weight_decay_factor=0)
+    model.compile(loss=F.cross_entropy, optimizer=optimizer, iterator_seed=1, cropped=cropped)
+    if cropped:
+        super_crop_size = np.round(train_dl.dataset.data.shape[2]/5)
+        model.fit(train_dl.dataset.data.numpy(), train_dl.dataset.target.numpy(), epochs=n_epochs, batch_size=64,
+                  scheduler='cosine', input_time_length=450, validation_data=(val_dl.dataset.data.numpy().squeeze(),
+                  val_dl.dataset.target.numpy().squeeze()))
+    else:
 
-    model.fit(train_dl.dataset.data.numpy(), train_dl.dataset.target.numpy(), epochs=1, batch_size=64, scheduler='cosine',
-              validation_data=(val_dl.dataset.data.numpy().squeeze(), val_dl.dataset.target.numpy().squeeze()),)
+        model.fit(train_dl.dataset.data.numpy(), train_dl.dataset.target.numpy(), epochs=n_epochs, batch_size=64,
+                  scheduler='cosine', validation_data=(val_dl.dataset.data.numpy().squeeze(),
+                                                       val_dl.dataset.target.numpy().squeeze()))
+
 
 
 
