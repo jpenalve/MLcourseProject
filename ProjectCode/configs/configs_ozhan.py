3743b9390425c9597eac0ba524079a69a7e7a09d
Özhan Özen <oezhan.oezen@artorg.unibe.ch>
2019-05-14 12:26:45 +0200

    Trying with more subject with EEGnet, it does not improve.

--- ProjectCode/configs/configs_ozhan.py
+++ ProjectCode/configs/configs_ozhan.py
@@ -116,8 +116,10 @@ class ConfigConvOzhan(DefaultConfig):
     
     config_name = 'ConvNetOzhan'
     config_remark = 'This is a cov NN test.. I am serious'
-    selected_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    #selected_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, \
+    #                    31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
 
+    selected_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
     
     nn_list = ['ConvNetOzhan']  # Extend if you want more. Add them in the nn_models_getter.py module
     nn_selection_idx = 0
@@ -127,6 +129,7 @@ class ConfigConvOzhan(DefaultConfig):
     weight_decay = 0.02
     
     
+    
     num_of_epochs = 50
     batch_size = 128
     use_early_stopping = False
@@ -134,7 +137,7 @@ class ConfigConvOzhan(DefaultConfig):
     
     normalize = True
     augment_with_gauss_noise = True
-    augmentation_factor = 4
+    augmentation_factor = 2
     
     
 # Put them all in a list
