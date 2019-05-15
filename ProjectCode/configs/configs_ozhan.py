221ea1c87921c8f0bcb74b1b62888c7e9ceca6ea
Özhan Özen <oezhan.oezen@artorg.unibe.ch>
2019-05-15 17:48:12 +0200

    Small dataset overfitting works. So we dont have a big problem with code

--- ProjectCode/configs/configs_ozhan.py
+++ ProjectCode/configs/configs_ozhan.py
@@ -54,29 +54,38 @@ class ConfigConvOzhan(DefaultConfig):
     
     config_name = 'ConvNetOzhan'
     config_remark = 'This is a cov NN test.. I am serious'
-    #selected_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, \
-    #                    31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
-
-    selected_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    selected_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+                         20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+                         30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+                         40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                         50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+                         60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+                         70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+                         80, 81, 82, 83, 84, 85, 86, 87,
+                         90, 91, 93, 94, 95, 96, 97, 98, 99,
+                         101, 102, 103, 104, 105, 106, 107, 108, 109]
+    nSubj = 80
+    selected_subjects = selected_subjects[:nSubj]
+    
     
     nn_list = ['ConvNetOzhan']  # Extend if you want more. Add them in the nn_models_getter.py module
     nn_selection_idx = 0
     optimizer_list = ['Adam']  # Extend if you want more. Add them in the optimizers.py module
     optimizer_selection_idx = 0  # Idx corresponds to entry optimizer_list (find below)
-    learning_rate = 1e-3
-    weight_decay = 0.01
+    learning_rate = 1e-2
+    weight_decay = 0.001
     
     scheduler = True  
     schStepSize = 10
     schGamma = 0.5
     
-    num_of_epochs = 40
+    num_of_epochs = 50
     batch_size = 128
     use_early_stopping = True
     es_patience = num_of_epochs
     
     normalize = True
-    augment_with_gauss_noise = True
+    augment_with_gauss_noise = False
     augmentation_factor = 2
     augment_std_gauss = 0.2
     
@@ -94,8 +103,8 @@ class ConfigConvOzhan(DefaultConfig):
     dropOutTimeOnly = False
     
     dropOutTilePerc = 0.5
-    dropOutTimeTile = 200
-    dropOutChannelTile = 8
+    dropOutTimeTile = 100
+    dropOutChannelTile = 4
     
     
 # Put them all in a list
